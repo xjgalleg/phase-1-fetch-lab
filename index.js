9f1fffc4de77161338ef57b1825569b2b4b31104
Josue Gallegos <gallegosjosue6@gmail.com>
2023-09-06 14:30:27 -0500

    cpmplete fetch lab

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
+  fetch("https://anapioficeandfire.com/api/books")
+  .then(resp => resp.json())
+  .then(json =>{renderBooks(json)
+  })
   
+  return fetch("https://anapioficeandfire.com/api/books")
 }
 
 function renderBooks(books) {
